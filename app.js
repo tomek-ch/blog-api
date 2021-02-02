6f952c84bb2f1b28522f127ab6ac33843e0957b4
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-02-02 17:57:41 +0100

    Set up db connection

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+const mongoDb = process.env.DB_KEY;
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Mongo connection error"));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
