6d5315a65674189c685038b2be65e585c5d26106
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-02-03 14:51:12 +0100

    Add error handling middleware

--- app.js
+++ app.js
@@ -14,5 +14,9 @@ mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongo connection error'));
 
+app.use((err, req, res, next) => {
+    res.sendStatus(500);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
