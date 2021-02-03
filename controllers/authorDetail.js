e6c9d73872db302d087cec531d5efa031def4ba2
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-02-03 18:57:31 +0100

    Validate author id on GET

--- controllers/authorDetail.js
+++ controllers/authorDetail.js
@@ -1,5 +1,19 @@
 const Author = require('../models/Author');
+const { param, validationResult } = require('express-validator');
 
-module.exports = async (req, res, next) => {
-    res.json(await Author.findById(req.params.id).catch(next));
-};
+module.exports = [
+
+    param('id', 'Invalid author id')
+        .isMongoId(),
+
+    async (req, res, next) => {
+
+        const errors = validationResult(req).array();
+        if (errors.length)
+            return res
+                .status(400)
+                .json([errors[0].msg]);
+
+        res.json(await Author.findById(req.params.id).catch(next));
+    },
+];
