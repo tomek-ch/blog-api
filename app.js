8d3d65f81fd0eb1376a8239440124df5cd3f8a97
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-02-03 10:41:41 +0100

    Fix quotes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const mongoDb = process.env.DB_KEY;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Mongo connection error"));
+db.on('error', console.error.bind(console, 'Mongo connection error'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
