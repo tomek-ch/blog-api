010b2283846aa8c12cb7761c01105fce4d149d7c
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-02-03 14:46:43 +0100

    Implement author details get

--- routes/router.js
+++ routes/router.js
@@ -3,8 +3,10 @@ const router = Router();
 
 const authorCreate = require('../controllers/authorCreate');
 const authorList = require('../controllers/authorList');
+const authorDetail = require('../controllers/authorDetail');
 
 router.get('/authors', authorList);
+router.get('/authors/:id', authorDetail);
 router.post('/authors', authorCreate);
 
 module.exports = router;
