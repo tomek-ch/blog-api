7a7b93e9487fb170cf5f25e6d13b0e1f88f16523
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-03-18 19:41:16 +0100

    Authorize post delete

--- controllers/post/postDelete.js
+++ controllers/post/postDelete.js
@@ -1,13 +1,20 @@
 const Post = require('../../models/Post');
 const Comment = require('../../models/Comment');
 const { param, validationResult } = require('express-validator');
+const auth = require('../../middleware/authenticate');
 
 module.exports = [
+
+    auth,
 
     param('id', 'Invalid post id')
         .isMongoId(),
 
     async (req, res, next) => {
+
+        const post = await Post.findById(req.params.id).catch(next);
+        if (req.user._id.toString() !== post.author.toString())
+            return res.sendStatus(403);
 
         const errors = validationResult(req).array();
         if (errors.length)
