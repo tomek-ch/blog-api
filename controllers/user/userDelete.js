952848a8b385e467c90e734c201ab03f0f02046c
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-04-01 21:29:34 +0200

    Authorize user delete

--- controllers/user/userDelete.js
+++ controllers/user/userDelete.js
@@ -3,16 +3,26 @@ const Post = require('../../models/Post');
 const Comment = require('../../models/Comment');
 const { param, validationResult } = require('express-validator');
 const auth = require('../../middleware/authenticate');
+const bcrypt = require('bcryptjs');
 
 module.exports = [
 
     auth,
 
     param('id', 'Invalid user id')
-        .isMongoId(),
+        .isMongoId().bail()
+        .custom(async (id, { req }) => {
+            try {
+                const user = await User.findById(id);
+                if (!await bcrypt.compare(req.body.password, user.password))
+                    return Promise.reject('Password is incorrect');
+            } catch (e) {
+                return Promise.reject('There was a network error');
+            }
+        }),
 
     async (req, res, next) => {
-
+        
         if (req.params.id !== req.user._id.toString())
             return res.sendStatus(403);
 
@@ -20,7 +30,7 @@ module.exports = [
         if (errors.length)
             return res
                 .status(400)
-                .json([errors[0].msg]);
+                .json(errors.map(err => err.msg));
 
         try {
             const usersPosts = (await Post.find({ user: req.params.id })).map(post => post._id);
