f42afd02d22a59c1a329a53317d77324f0b10854
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-04-04 21:24:34 +0200

    Delete replies on post delete

--- controllers/post/postDelete.js
+++ controllers/post/postDelete.js
@@ -1,5 +1,6 @@
 const Post = require('../../models/Post');
 const Comment = require('../../models/Comment');
+const Reply = require('../../models/Reply');
 const { param, validationResult } = require('express-validator');
 const auth = require('../../middleware/authenticate');
 
@@ -11,22 +12,29 @@ module.exports = [
         .isMongoId(),
 
     async (req, res, next) => {
-
-        const post = await Post.findById(req.params.id).catch(next);
-        if (req.user._id.toString() !== post.author.toString())
-            return res.sendStatus(403);
-
-        const errors = validationResult(req).array();
-        if (errors.length)
-            return res
-                .status(400)
-                .json([errors[0].msg]);
-
-        const [deletedPost] = await Promise.all([
-            Post.findByIdAndDelete(req.params.id),
-            Comment.deleteMany({ post: req.params.id }),
-        ]).catch(next);
-
-        res.json(deletedPost);
+        try {
+            const post = await Post.findById(req.params.id);
+            if (req.user._id.toString() !== post.author.toString())
+                return res.sendStatus(403);
+    
+            const errors = validationResult(req).array();
+            if (errors.length)
+                return res
+                    .status(400)
+                    .json([errors[0].msg]);
+    
+            const comments = await Comment.find({ post: req.params.id });
+            const replyIds = comments.reduce((ids, com) => [...ids, ...com.replies], []);
+    
+            const [deletedPost] = await Promise.all([
+                Post.findByIdAndDelete(req.params.id),
+                Comment.deleteMany({ post: req.params.id }),
+                Reply.deleteMany({ _id: { $in: replyIds } }),
+            ]);
+    
+            res.json(deletedPost);
+        } catch (e) {
+            next(e);
+        }
     },
 ];
