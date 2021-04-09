1e44e3e6947e8a7946fcf6d62b2be00f1383ab89
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-04-09 23:01:21 +0200

    Fix post list author parameter

--- controllers/post/postList.js
+++ controllers/post/postList.js
@@ -1,9 +1,9 @@
 const Post = require('../../models/Post');
 
 module.exports = async (req, res, next) => {
-    const { user, tags } = req.query;
+    const { author, tags } = req.query;
     try {
-        const posts = await Post.find({ user, tags }).populate('author');
+        const posts = await Post.find({ author, tags }).populate('author');
         res.json(posts);
     } catch (e) {
         next(e);
