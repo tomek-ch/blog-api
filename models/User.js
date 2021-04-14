0187208a0982536fa77dc9c28fa2d922e0471f9b
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-04-14 22:06:58 +0200

    Add max lengths to user model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const { Schema, model } = require('mongoose');
 
 const User = new Schema({
-    firstName: { type: String, required: true },
-    lastName: String,
-    description: String,
+    firstName: { type: String, required: true, maxLength: 15 },
+    lastName: { type: String, maxLength: 15 },
+    description: { type: String, maxLength: 100 },
     password: { type: String, required: true},
-    username: { type: String, required: true},
+    username: { type: String, required: true, maxLength: 15},
 });
 
 module.exports = model('User', User);
