1186ea775e4bd29dfd591095dc124016de1730d5
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-05-16 20:44:25 +0200

    Remove comment id validation from comment list controller

--- controllers/comment/commentList.js
+++ controllers/comment/commentList.js
@@ -1,14 +1,8 @@
 const Comment = require('../../models/Comment');
-const { ObjectId } = require('mongoose').Types;
 
 module.exports = async (req, res, next) => {
     try {
         const { comment, author } = req.query;
-        if (comment && !ObjectId.isValid(comment))
-            return res
-                .status(400)
-                .json(['Invalid comment id']);
-
         const comments = await Comment.find({ comment, author })
             .populate('author')
             .populate('comment');
