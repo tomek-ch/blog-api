225c2ae0ab1b0a73d92ee3e17234b184842d0d2e
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-05-18 13:25:18 +0200

    Validate comment list query params

--- controllers/comment/commentList.js
+++ controllers/comment/commentList.js
@@ -1,14 +1,33 @@
 const Comment = require('../../models/Comment');
+const { query, validationResult } = require('express-validator');
 
-module.exports = async (req, res, next) => {
-    try {
-        const { comment, author } = req.query;
-        const comments = await Comment.find({ comment, author })
-            .populate('author')
-            .populate('comment');
+module.exports = [
 
-        return res.json(comments);
-    } catch (e) {
-        return next(e);
-    }
-};
+    query('comment')
+        .optional()
+        .isMongoId(),
+
+    query('author')
+        .optional()
+        .isMongoId(),
+
+    async (req, res, next) => {
+
+        const errors = validationResult(req).array();
+        if (errors.length)
+            return res
+                .status(400)
+                .json(errors.map(err => err.msg));
+
+        try {
+            const { comment, author } = req.query;
+            const comments = await Comment.find({ comment, author })
+                .populate('author')
+                .populate('comment');
+
+            return res.json(comments);
+        } catch (e) {
+            return next(e);
+        }
+    },
+];
